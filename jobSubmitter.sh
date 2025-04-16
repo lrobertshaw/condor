946fe36ddcb0f21329e8627cd9e0d6cf5c9f9cbb
Liam Robertshaw <liam.robertshaw@cern.ch>
2025-04-16 17:18:01 +0200

    created new branch for creating slimmed inputs from TPs

--- jobSubmitter.sh
+++
@@ -1,4 +0,0 @@
-jobName="Phase2Spring24_SingleNeutrino_PU200"
-inputFiles="/eos/cms/store/cmst3/group/l1tr/FastPUPPI/14_2_X/fpinputs_140X/v0/SingleNeutrino_PU200/"
-
-python3 condorSub.py "$jobName" "$inputFiles"
